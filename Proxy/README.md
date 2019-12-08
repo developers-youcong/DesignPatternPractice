4c6c8508fa9a74a7ebcebf364dbff0a9a94ac45f
developers-youcong <15200740587@163.com>
2019-12-08 19:53:36 +0800

    补充提交代理模式例子

---
+++ Proxy/README.md
@@ -0,0 +1,4 @@
+# 代理模式
+
+## 包说明
+com.proxy.common.example(代理模式的模板)
---
+++ Proxy/src/main/java/com/proxy/common/example/RealSubject.java
@@ -0,0 +1,13 @@
+package com.proxy.common.example;
+
+/**
+ * 真实主题类
+ */
+public class RealSubject implements Subject {
+
+    //实现方法
+    public void request() {
+
+        //业务逻辑处理
+    }
+}
