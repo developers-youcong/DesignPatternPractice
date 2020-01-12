3d4b9fa14d08e5f50e89a1bfdcd4102d4c1112ed
developers-youcong <15200740587@163.com>
2020-01-12 19:38:47 +0800

    feat:新增责任链模式例子

---
+++ ChainOfResponsibility/README.md
@@ -0,0 +1,4 @@
+# 责任链模式
+
+## 包说明
+com.ChainOfResponsibility.common.example 责任链模式例子(以古代妇女逛街为例)
---
+++ ChainOfResponsibility/src/main/java/com/ChainOfResponsibility/common/example/Father.java
@@ -0,0 +1,19 @@
+package com.ChainOfResponsibility.common.example;
+
+public class Father extends Handler {
+
+    public Father() {
+        super(Handler.FATHER_LEVEL_REQUEST);
+    }
+
+
+    protected void response(IWomen women) {
+
+        System.out.println("---女儿向父亲请示---");
+
+        System.out.println(women.getRequest());
+
+        System.out.println("父亲的答复是:同意\n");
+
+    }
+}
