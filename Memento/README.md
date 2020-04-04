fb14422942582ffd65612cb4a95abcd4e567b63a
developers-youcong <15200740587@163.com>
2020-04-04 10:39:11 +0800

    feat 增加备忘录模式例子

---
+++ Memento/README.md
@@ -0,0 +1,4 @@
+# 备忘录模式
+
+## 包说明
+com.memento.common.example(备忘录模式通用例子)
---
+++ Memento/src/main/java/com/memento/common/example/Client.java
@@ -0,0 +1,24 @@
+package com.memento.common.example;
+
+/**
+ * 场景类
+ */
+public class Client {
+
+
+    public static void main(String[] args) {
+
+        //定义发起人
+        Originator originator = new Originator();
+
+        //定义出备忘录管理员
+        Caretaker caretaker = new Caretaker();
+
+
+        //创建一个备忘录
+        caretaker.setMemento(originator.createMemento());
+
+        //恢复一个备忘录
+        originator.restoreMemento(caretaker.getMemento());
+    }
+}
